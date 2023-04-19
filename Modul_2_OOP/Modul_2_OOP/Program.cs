0a6c02d3d8d623f8b9f4a8d6ef81fd730539dda7
Tursunboy <tursunboyrahimjonov1@gmail.com>
2023-04-19 10:08:58 +0500

    OOP_class

--- Modul_2_OOP/Modul_2_OOP/Program.cs
+++ Modul_2_OOP/Modul_2_OOP/Program.cs
@@ -10,6 +10,26 @@ namespace Modul_2_OOP
     {
         static void Main(string[] args)
         {
+           Student student = new Student(12,"Tursunboy", "Raximjonov",2,5);
+
+            Console.WriteLine(" ------------- 1  -------------------");
+
+            Console.WriteLine(student.Id);
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.Lastname);
+            Console.WriteLine(student.Group);
+            Console.WriteLine(student.Grade);
+
+            Console.WriteLine(" ----------------------- 2 ------------------");
+
+            Student student1 = new Student();
+
+            Console.WriteLine(student1.Id);
+            Console.WriteLine(student1.Name);
+            Console.WriteLine(student1.Lastname);
+
+            Console.ReadLine();
         }
+
     }
 }
---
+++ Modul_2_OOP/Modul_2_OOP/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_2_OOP
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Group { get; set; }
+        public int Grade { get; set; }
+
+        public Student(int id, string name, string lastname, int group, int grade)
+        {
+            Id = id;
+            Name = name;
+            Lastname = lastname;
+            Group = group;
+            Grade = grade;
+
+        }
+
+        public Student(int id, string name, string lastname)
+        {
+            Id = id;
+            Name = name;
+            Lastname = lastname;
+        }
+        public Student()
+        {
+            Id = 12;
+            Name = "Tursunboy";
+            Lastname = "Raximjonov";
+            Group = 1;
+            Grade = 5;
+        }
+    }
+}
